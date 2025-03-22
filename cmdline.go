ebaa544545b4695552730919192e366b95a87629
kenyblvk <ezeilokenechukwu@gmail.com>
2025-03-22 16:41:52 +0000

    working function

---
+++ cmdline.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+func getName(r io.Reader, w io.Writer) (string, error) {
+	msg := "Your name please? Press the Enter key when done.\n"
+	fmt.Fprintf(w, msg)
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+	name := scanner.Text()
+	if len(name) == 0 {
+		return "", errors.New("You didn't enter your name")
+	}
+	return name, nil
+}
+
+type config struct {
+	numTimes   int
+	printUsage bool
+}
+
+func parseArgs(args []string) (config, error) {
+	var numTimes int
+	var err error
+	c := config{}
+	if len(args) != 1 {
+		return c, errors.New("Invalid number of arguments")
+	}
+
+	if args[0] == "-h" || args[0] == "--help" {
+		c.printUsage = true
+		return c, nil
+	}
+
+	numTimes, err = strconv.Atoi(args[0])
+	if err != nil {
+		return c, err
+	}
+	c.numTimes = numTimes
+
+	return c, nil
+}
+func validateArgs(c config) error {
+	if !(c.numTimes > 0) {
+		return errors.New("Must specify a number greater than 0")
+	}
+	return nil
+}
+func runCmd(r io.Reader, w io.Writer, c config) error {
+	if c.printUsage {
+		printUsage(w)
+		return nil
+	}
+
+	name, err := getName(r, w)
+	if err != nil {
+		return err
+	}
+	greetUser(c, name, w)
+	return nil
+}
+func greetUser(c config, name string, w io.Writer) {
+	msg := fmt.Sprintf("Nice to meet you %s\n", name)
+	for i := 0; i < c.numTimes; i++ {
+		fmt.Fprintf(w, msg)
+	}
+}
